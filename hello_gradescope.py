b22f66d980ac6c0b3b06bcd843280d6731045de5
Noobyz <zorlosky@gmail.com>
2022-01-07 13:16:27 -0800

    Initial commit

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Zachary Orlosky
+# OSU Email: orloskyz@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: Assignment 0
+# Due Date: 01/12/2022
+# Description: test
+
+
+def gradescope_test():
+    return 42
