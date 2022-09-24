c95e7d8004982ca6be38e29122fabb998922a2a8
zorlo <orloskyz@oregonstate.edu>
2022-09-23 20:27:44 -0700

    ok

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Zach Orlosky
+# OSU Email: Orloskyz@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 0
+# Due Date: 03 Oct 2022
+# Description: Return 42
+
+
+def gradescope_test():
+    return 42
